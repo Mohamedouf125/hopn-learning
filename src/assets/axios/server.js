10b55bdfcd409385df64a07b5400f0a00a11ce05
ibrahim-mohamed-rezk <ibrahimmo989800@gmail.com>
2025-09-03 20:07:30 +0300

    Update axios base URL to point to the new learning API endpoint

--- src/assets/axios/server.js
+++ src/assets/axios/server.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const lang = localStorage.getItem("lang");
 
 const server = axios.create({
-  baseURL: "https://api.sportiin.com/api/v1",
+  baseURL: "https://api.learning.hopn.eu/api/v1",
   headers: {
     lang: lang || "en",
   },
